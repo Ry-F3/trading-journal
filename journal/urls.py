2437320e65b7d8d89dc3718e9e8f014bd91a7091
Ry-F3 <rhysfeew@gmail.com>
2023-12-13 19:28:08 +0000

    Fix form not posting resulting in 404 error

--- journal/urls.py
+++ journal/urls.py
@@ -5,5 +5,6 @@ from .views import trade_list
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('trade_list/', trade_list, name='trade_list'),
+    path('trade_list/post/', trade_list, name='trade_list_post'),
    
 ]
