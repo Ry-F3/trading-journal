0c994f9b8a21acce40b9022ae48f90fdf9ca4798
Ry-F3 <rhysfeew@gmail.com>
2024-01-11 16:11:47 +0000

    Tootltip fade no working on deployment - test as inline scriptin base.html

--- static/js/tooltip.js
+++
@@ -1,6 +0,0 @@
-  // JavaScript to close alerts after a delay
-  window.setTimeout(function () {
-    $(".alert").fadeTo(300, 0).slideUp(300, function () {
-        $(this).remove();
-    });
-}, 3500);
